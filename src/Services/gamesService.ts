54a1233d4953e89c65db3a17675245289fe8e425
Netanel Rotenstein <email@example.com>
2024-12-04 18:42:19 +0200

    fix populate

--- src/Services/gamesService.ts
+++ src/Services/gamesService.ts
@@ -1,23 +1,24 @@
+import mongoose from "mongoose";
 import gameModel from "../Models/gameModel"
 import IGame from "../types/interfaces/IGame"
+import '../Models/teamModel'
+
 
 export const allGamesService = async () => {
     try {
-        const games = await gameModel.find({}).populate('games')
+        const games = await gameModel.find({}).populate("teamA").populate("teamB")
         return games
     } catch (error) {
-        console.log(`can't register`)
+        console.log(error)
     }
 }
 
 export const addGameService = async (game: IGame) => {
     try {
         const { teamA, teamB, dateOfGame, ended, scoreA, scoreB, bets } = game
-        const newGame =  new gameModel({teamA, teamB, dateOfGame, ended, scoreA, scoreB, bets})
+        const newGame = new gameModel({ teamA, teamB, dateOfGame, ended, scoreA, scoreB, bets })
         return await newGame.save()
     } catch (error) {
         console.log(error)
     }
-    
 }
- 
