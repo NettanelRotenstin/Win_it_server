1bf1b3c13f8bed319a1450c6bea64a4782521f19
Netanel Rotenstein <email@example.com>
2024-12-08 10:47:14 +0200

    connect save to db in bet socket

--- src/Socket/io.ts
+++ src/Socket/io.ts
@@ -1,11 +1,13 @@
 import { Socket } from "socket.io";
 import betDTO from "../types/DTO/betDto";
 import { historyBetsOfUser } from "./funcsForSocket";
+import { betService } from "../Services/betService";
  
 
  export const handelSocketConnection = async (client: Socket) => {
   //when client bet he will get all his bets
   client.on("bet", async (newbet:betDTO) => {
+      await betService(newbet)
      client.emit('all-bets',await historyBetsOfUser(newbet.userId))
   });
 };
